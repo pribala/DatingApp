385940aea37bbf22ddc422a65b0696857b36fec2
Priya Balakrishnan <andu_pri@yahoo.com>
2024-01-18 22:39:20 -0500

    initial commit

---
+++ DatingAPI/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using DatingAPI.Data;
+using DatingAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id) {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
